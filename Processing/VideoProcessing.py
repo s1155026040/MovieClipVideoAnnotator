1e26a9bc55e69fdf99a8a89af90acfe001a171b3
salmedina <salbed.medina@gmail.com>
2016-03-21 21:47:30 -0400

    Add Processing folder, this folder will contain scripts tht preprocess the video before heading to a machine learning model

---
+++ Processing/VideoProcessing.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# 
+# This is a set of tools that process a video and extracts the
+# keyfames based on different criteria such as random sampling
+# color histogram and motion detection
+#
+# Carnegie Mellon University
+# author: Salvador Medina
+# last update: 2016-03-19
+#
+
+import cv2
+
+def random_extract(video_path, num_frames):
+    
+    pass
+
+def color_extract(video_path, num_frames):
+    pass
+
+def motion_extract(video_path, num_frames):
+    pass
