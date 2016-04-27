0492620c2986f374a2d99aab666310b9a9f02d44
Salvador Medina <zal@atlas4.multicomp.cs.cmu.edu>
2016-04-27 08:29:57 -0400

    Fixed Generator on server

--- GIFGenerator/Generator.py
+++ GIFGenerator/Generator.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import VideoToGif as v2g
 import AnnotationDB as adb
 import rawdataLoad as rdl
@@ -9,12 +10,17 @@ import multiprocessing as mp
 DATASETS_PATH = '/multicomp/datasets/'
 GIF_PATH = '/home/zal/dataset_gifs'
 
-# Load the data
-raw_data = rdl.main(0)
-
 # Connect to the DB
 db = adb.AnnotationDB()
-db.init('atlas4.multicomp.cs.cmu.edu', 'annotator', '', 'annodb')
+connected = db.init('atlas4.multicomp.cs.cmu.edu', 'annotator', 'multicomp', 'annodb')
+if not connected:
+    print 'Could not connect to DB, closing program'
+    sys.exit(-1)
+
+print db.get_all_users()
+
+# Load the data
+raw_data = rdl.main(0)
 
 # Start exporting the GIF files
 N = 10;
@@ -23,8 +29,9 @@ for caption, video_path in raw_data.test:
 
     try:
         caption_id = db.get_caption_id(video_path)
+        gif_fn = '%06d.gif'%(caption_id)
         v2g.video_to_gif(os.path.join(DATASETS_PATH,video_path), 
-                     os.path.join(GIF_PATH, gif_path), resize=[427,240])
+                         os.path.join(GIF_PATH, gif_fn), resize=[427,240])
     except AttributeError:
         print 'There is no caption ID for %s'%(video_path)
         
