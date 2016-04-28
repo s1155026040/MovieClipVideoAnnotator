550cccf96743221805035c925144b701dcde6c81
salmedina <salbed.medina@gmail.com>
2016-04-28 08:57:54 -0400

    Enabled to run on the whole test set

--- Tools/GenerateNeuralTalkTestFile.py
+++ Tools/GenerateNeuralTalkTestFile.py
@@ -62,7 +62,7 @@ raw_data = rdl.main(0)
 images_field = []
 annotations_field = []
 
-for caption, video_path in raw_data.test[1:10]:
+for caption, video_path in raw_data.test[]:
     # Get the caption id
     caption_id = int(db.get_caption_id(video_path))
     video_name = os.path.splitext(os.path.basename(video_path))[0]
